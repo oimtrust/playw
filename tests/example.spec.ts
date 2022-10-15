7fc5a172443e2ff5d3215275ea6b634351e22d9c
Fathur Rohim <trustoim@gmail.com>
2022-10-15 21:11:59 +0700

    click an element

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -8,5 +8,9 @@ test('Simple basic test', async ({page}) => {
 
 test('Clicking on Element', async ({page}) => {
     await page.goto('http://zero.webappsecurity.com/')
-    
+    await page.click('#signin_button')
+    await page.click('name="submit"')
+
+    const errorMessage = await page.locator('.alert-error')
+    await expect(errorMessage).toContainText('Login and/or password are wrong.')
 });
