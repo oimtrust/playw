1bac20540f9f976bfcd452abfe277157514459db
Fathur Rohim <trustoim@gmail.com>
2022-11-10 00:33:16 +0700

    tagging

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -9,7 +9,22 @@ test('Simple basic test', async ({page}) => {
 test('Clicking on Element', async ({page}) => {
     await page.goto('http://zero.webappsecurity.com/')
     await page.click('#signin_button')
-    await page.click('name="submit"')
+    await page.click('[name="submit"]')
+
+    const errorMessage = await page.locator('.alert-error')
+    await expect(errorMessage).toContainText('Login and/or password are wrong.')
+});
+
+test('Clicking on Input @click-my-tag', async ({page}) => {
+    /**
+     * type command for run test
+     * npx playwright test --grep @click-my-tag
+     */
+    await page.goto('http://zero.webappsecurity.com/')
+    await page.click('#signin_button')
+    await page.type('#user_login', 'username')
+    await page.type('#user_password', 'user_password')
+    await page.click('[name="submit"]')
 
     const errorMessage = await page.locator('.alert-error')
     await expect(errorMessage).toContainText('Login and/or password are wrong.')
