d1a08f0448caed7856a736fd055f05c22e533b75
Fathur Rohim <trustoim@gmail.com>
2025-03-01 00:13:48 +0700

    get

--- restapi/tests/api.spec.ts
+++ restapi/tests/api.spec.ts
@@ -5,10 +5,24 @@ test.describe.parallel('API tests', () => {
   test('Assert Response Status', async ({ request }) => {
     const response = await request.get(`${baseURL}/users/2`);
       expect(response.status()).toBe(200);
+      /**
+       * Parse Response Body
+       */
+      const responseBody = JSON.parse(await response.text());
+      console.log(responseBody);
   });
   
   test('Assert Invalid Endpoint', async ({ request }) => {
     const response = await request.get(`${baseURL}/users/invalid`);
       expect(response.status()).toBe(404);
+  });
+
+  test('Get User Detail', async ({ request }) => {
+    const response = await request.get(`${baseURL}/users/2`);
+    const responseBody = JSON.parse(await response.text());
+    expect(responseBody.data.id).toBe(2);
+    expect(responseBody.data.email).toBe('janet.weaver@reqres.in');
+    expect(responseBody.data.first_name).toBe('Janet');
+    expect(responseBody.data.last_name).toBe('Weaver');
   });
 });
