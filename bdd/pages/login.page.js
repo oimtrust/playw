8c4824a2a8416d40c8552bb45b1a64d81e359d06
Fathur Rohim <trustoim@gmail.com>
2025-03-02 01:06:53 +0700

    scenario outline

--- bdd/pages/login.page.js
+++ bdd/pages/login.page.js
@@ -8,12 +8,23 @@ class LoginPage {
         await page.type('#password', password);
     }
 
+    async inputInvalidAccount(username, password) {
+        await page.type('#user-name', username);
+        await page.type('#password', password);
+    }
+
     async clickLoginButton() {
         await page.click('#login-button');
     }
 
     async assertUserIsLoggedIn() {
         await page.waitForSelector('.inventory_list');
+    }
+
+    async assertErrorMessage(message) {
+        await page.waitForSelector('[data-test="error"]');
+        const errorMessage = await page.$eval('[data-test="error"]', (el) => el.innerText);
+        expect(errorMessage).contain(message)
     }
 }
 
--- bdd/steps/login.step.js
+++ bdd/steps/login.step.js
@@ -1,4 +1,4 @@
-const { Given, When, Then } = require('@cucumber/cucumber');
+const { Given, When, Then, defineStep} = require('@cucumber/cucumber');
 const { LoginPage } = require('../pages/login.page');
 
 const loginPage = new LoginPage();
@@ -11,10 +11,24 @@ When('I enter valid credentials', async function () {
     await loginPage.inputValidAccount('standard_user', 'secret_sauce');
 });
 
+/**
+ * Regular expression can be used to pass parameters to the step definition
+ */
+defineStep(
+    /^I enter "([^"]*)" and "([^"]*)"$/,
+    async function (username, password) {
+        await loginPage.inputValidAccount(username, password);
+    }
+)
+
 When('I click on the login button', async function () {
     await loginPage.clickLoginButton();
 });
 
 Then('I should be redirected to the dashboard page', async function () {
     await loginPage.assertUserIsLoggedIn();
+});
+
+Then('I should see an error message', async function () {
+    await loginPage.assertErrorMessage('Epic sadface: Username and password do not match any user in this service');
 });
