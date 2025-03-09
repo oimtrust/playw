6651c4694393c0b34534cde0a96d5e6790960dce
Fathur Rohim <trustoim@gmail.com>
2025-03-09 14:04:06 +0700

    add locator and pages

--- playslide/data/auth.data.ts
+++ playslide/data/auth.data.ts
@@ -24,6 +24,8 @@ export const AuthData = {
 
 export const Messages = {
     LOGIN_ERROR: 'Epic sadface: Username and password do not match any user in this service',
+    PASSWORD_REQUIRED_ERROR: 'Epic sadface: Password is required',
+    USERNAME_REQUIRED_ERROR: 'Epic sadface: Username is required',
     LOCKED_OUT_ERROR: 'Epic sadface: Sorry, this user has been locked out.',
     PROBLEM_USER_ERROR: 'Epic sadface: Sorry, this user is not allowed to log in.',
     PERFORMANCE_GLITCH_ERROR: 'Epic sadface: Well, this is embarrassing. Something went wrong.',
---
+++ playslide/locator/cart.locator.ts
@@ -0,0 +1,3 @@
+export const TestID = {
+    CHECKOUT_BUTTON: '#checkout'
+}
