f1fece06958687d3b646709fc41686eefa8d0230
Fathur Rohim <trustoim@gmail.com>
2025-03-09 17:17:50 +0700

    add checkout page and deleted useless test file

--- playslide/tests/checkout.spec.ts
+++
@@ -1,8 +0,0 @@
-import { test } from '@playwright/test';
-import { login, checkout } from '../utils/helpers';
-
-test('Melakukan Checkout', async ({ page }) => {
- await login(page, 'standard_user', 'secret_sauce'); // Login terlebih dahulu
- await page.click('.inventory_item button'); // Klik tombol "Add to cart"
- await checkout(page); // Proses checkout
-});
--- playslide/tests/logout.spec.ts
+++
@@ -1,7 +0,0 @@
-import { test } from '@playwright/test';
-import { login, logout } from '../utils/helpers';
-
-test('Logout dari SauceDemo', async ({ page }) => {
-    await login(page, 'standard_user', 'secret_sauce');
-    await logout(page);
-});
